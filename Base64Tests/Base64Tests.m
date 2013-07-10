cd1eed4096c8f2a84e7ff15da92e8f7d4eb970da
Benjamin Borbe <bborbe@rocketnews.de>
2013-07-10 14:42:44 +0200

    update license

--- Base64Tests/Base64Tests.m
+++ Base64Tests/Base64Tests.m
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2013, Benjamin Borbe
+//  Copyright (c) 2013, Benjamin Borbe <bborbe@rocketnews.de>
 //  All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
